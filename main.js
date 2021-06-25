07d1ec7306202d8c3091489eca6f166985661287
Aldo Quintero <aldoq95@gmail.com>
2021-06-24 18:44:35 -0700

    2nd api updated and working

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //place holder for now
 var requestUrl = "https://pokeapi.co/api/v2/pokemon?limit=151"
-var searchBarData = document.querySelector('.btn')
-var pokemonInfo;
+var searchBarData = document.querySelector('.button')
+
 
 async function getApi(requestUrl){
   fetch(requestUrl)
@@ -13,10 +13,12 @@ async function getApi(requestUrl){
 }
 // event listener that takes user input and displays data of pokimon in web console 
 getApi(requestUrl);
+
+
 searchBarData.addEventListener('click',function(event){
   event.preventDefault();
-  var textInput = document.querySelector('.textInput').value
-  // console.log(textInput);
+  var textInput = document.querySelector('.input').value
+ 
  requestUrl =`https://pokeapi.co/api/v2/pokemon/${textInput}`
   // console.log(data);
   // fetched pokemon data
@@ -27,35 +29,36 @@ searchBarData.addEventListener('click',function(event){
   })
   .then (function(dataApi_1){
     console.log(dataApi_1);
-    // pokemonInfo = 
-
+    // search history is passing the data on line 50-55 the parameter naming doesnt matter it wil be the same data
   })
-  // 2nd api call with user input
-requestUrl_2 = `https://api.pokemontcg.io/v2/cards?q=name:${textInput}&appid=dbd45731-4537-4e36-83cf-77eafd972ad5`
-fetch(requestUrl_2)
-  .then(function (response){
-    return response.json()
-  })
-  // clg api 2nd api data
-  .then (function(dataApi_2){
-    console.log(dataApi_2);
-
-    
-  })
+  //passing textInput from eventListener to pokemonApi_2 #1 pass
+  pokemonApi_2(textInput)
 })
+// passing api key through header
 
-// function searchHistory(dataApi_2,dataApi_1){
- 
-// }
-// searchHistory()
+//textInput is a parameter that is being passed 
 
-
-
-
-
-
-// tcg api
-// https://api.pokemontcg.io/v2/cards?q=name:grookey
+// #4 textInput now exist in api function to call the data via event listener 
+async function api(textInput){
+    let response = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${textInput}`,{
+      method: "GET",
+      headers: {
+        'x-api-key':"dbd45731-4537-4e36-83cf-77eafd972ad5"
+      }
+    })
+    let data = await response.json();
+    return data
+  }
+ // #2 pass textInput is now in function pokemonApi_2
+  function pokemonApi_2(textInput) {
+  //#3 pass now textInput is being passing into api
+  // .then(response is holding the api data)
+  // clg will display the photo 
+    api(textInput).then(response=>{
+      console.log(response.data[0].artist);
+       artistName =response.data[0].artist
+    })
+  } 
 
 
 
