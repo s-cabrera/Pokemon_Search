99370c8da48b65be91ea87c2a596b2f6b260a37b
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-06-28 17:56:00 -0700

    Updated merged code

--- script.js
+++ script.js
@@ -11,11 +11,6 @@ var pokeSprite = $('#sprite');
 var pokeName = $('.title');
 var pokeType = $('.subtitle');
 var pokeFlavor = $('.content');
-var searches = Array(0);
-var pokemon;
-
-console.log(localStorage);
-// localStorage.clear();
 
 function addSearchItem(name, img) {
     let a = $('<a class="panel-block is-active">');
@@ -30,10 +25,43 @@ function addSearchItem(name, img) {
     a.append(p);
 }
 
-/////////////////////////////////////////////////////////////////////
-//--------  Flavor text Poke API call ---------// 
+async function apiCall(pokemon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}/`)
+        .then(function (response) {
+            if (!response.ok) {
+                $('.invalid').removeClass('is-hidden');
+                return 0;
+            }
+            $('.invalid').addClass('is-hidden');
+            return response.json();
+        })
+        .then(async function (data) {
+            searchBtn1El.removeClass('is-loading');
+            searchBtn2El.removeClass('is-loading');
+            if (!data) {
+                console.log("No data recorded")
+                return;
+            }
+
+            //Show the sidebar and loading icon. Hide the initSearchBar
+            initSearchBar.addClass('is-hidden');
+            $('#loading-icon').removeClass('is-hidden');
+            $('#display').removeClass('is-12');
+            $('#display').removeClass('is-8');
+            recentSearch.removeClass('is-hidden');
+
+            var name = data.name;
+            name = capitalizeFirstLetter(name);
+            displayInfo(data, name);
+            addSearchItem(name, data.sprites.front_default);
+            var flavorText = await apiCallAgain(data.name);
+            var tcgCard = await pokemonApi_2(data.name);
+            savePokemon(data, flavorText, tcgCard);
+        });
+};
+
 async function apiCallAgain(pokemon) {
-    //Promise(function(resolve, reject) {
+    return new Promise(function(resolve, reject) {
         fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.toLowerCase()}/`)
         .then(function (response) {
             if (!response.ok) {
@@ -46,14 +74,12 @@ async function apiCallAgain(pokemon) {
             if (!flavor) {
                 console.log("No data recorded");
                 return;
-            }
-            console.log("Flavor");
-            console.log(flavor);
+            };
             displayFlavor(flavor);
-            return flavor;//resolve(flavor);
+            return resolve(flavor);
         });
-    //})
-}
+    })
+};
 
 async function api(textInput) {
     let response = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${textInput}`, {
@@ -65,76 +91,21 @@ async function api(textInput) {
     let data = await response.json();
     return data
 }
-/////////////////////////////////////////////////////////////////////
-
-//---------  Poke API call ------------//
-async function apiCall(pokemon){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}/`)
-  .then(function(response){
-    if (!response.ok) {
-        $('.invalid').removeClass('is-hidden');   
-        return 0;
-    }
-    $('.invalid').addClass('is-hidden');
-    return response.json();
-  })
-  .then(function(data){
-      searchBtn1El.removeClass('is-loading');
-      searchBtn2El.removeClass('is-loading');
-      if(!data){
-          console.log("No data recorded")
-          return;
-        }
-        //Show the sidebar and loading icon. Hide the initSearchBar
-        initSearchBar.addClass('is-hidden');
-        $('#loading-icon').removeClass('is-hidden');
-        $('#display').removeClass('is-12');
-        $('#display').removeClass('is-8');
-        recentSearch.removeClass('is-hidden');
-
-        //Set variables and store data
-        console.log(data);
-        id = data.id;
-        var name = data.name;
-        name = name[0].toUpperCase() + name.slice(1, (name.length));
-        name = capitalizeFirstLetter(name);
-        searches.push(name);
-
-        //Add Search Item
-        addSearchItem(name, data.sprites.front_default);
-        
-        //API calls
-        var flavorText = apiCallAgain(data.name);
-        console.log(flavorText);
-        var tcgCard = pokemonApi_2(data.name);
-        console.log(tcgCard);
-        savePokemon(data, flavorText, tcgCard);
-        
-        //Update the display card info
-        displayInfo(data, name);
-    });
-}
-
 
 //--------  Flavor text API call ---------// 
-  function pokemonApi_2(textInput) {
-    //return new Promise(function(resolve, reject) {
+function pokemonApi_2(textInput) {
+    return new Promise(function(resolve, reject) {
     api(textInput)
         .then(response => {
             displayCard(response);
-
-            //Display the Pokemon card and hide the loading icon AFTER image is set
-            $('#loading-icon').addClass('is-hidden');
-            infoCard.removeClass('is-hidden');
-            
-            return response;//resolve(response);
+            return resolve(response);
         })
-    //})
+    })
 }
 
-//----------------- Display Flavor Text ---------------//
 function displayFlavor(flavorApi) {
-    let flav = flavorApi.flavor_text_entries[1].flavor_text;
+    console.log(flavorApi);
+    let flav = flavorApi.flavor_text_entries[0].flavor_text;
     for(let i = 0; i < flavorApi.flavor_text_entries.length; i++){
         if(flavorApi.flavor_text_entries[i].language.name === "en"){
             flav = flavorApi.flavor_text_entries[i].flavor_text;
@@ -144,7 +115,6 @@ function displayFlavor(flavorApi) {
     pokeFlavor.text(flav);
 }
 
-//---------------- Display TCG Card -------------------//
 function displayCard(cardApi) {
     let image = cardApi.data[1].images.large;
     displayImageEl.attr('src', image);
@@ -153,7 +123,6 @@ function displayCard(cardApi) {
     infoCard.removeClass('is-hidden');
 }
 
-//------------ Display Pokemon Card Info -------------//
 function displayInfo(api, name) {
     let icon = api.sprites.front_default;
     let type = api.types[0].type.name;
@@ -173,8 +142,8 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-var searchBtn1Handler = function (event) {
-    console.log($(event.target).attr('id'));
+  var searchBtn1Handler = function (event) {
+    //console.log($(event.target).attr('id'));
     if (searchInput1El.val().length > 0) {
         searchBtn1El.addClass('is-loading');
         apiCall(searchInput1El.val().trim());
@@ -182,7 +151,7 @@ var searchBtn1Handler = function (event) {
 }
 
 var searchBtn2Handler = function (event) {
-    console.log($(event.target).html());
+    //console.log($(event.target).html());
     if (searchInput2El.val().length > 0) {
      infoCard.addClass('is-hidden');
      searchBtn2El.addClass('is-loading');
@@ -195,15 +164,24 @@ searchBtn1El.on('click', searchBtn1Handler);
 searchBtn2El.on('click', searchBtn2Handler);
 recentSearch.on('click', function (event) {
     if (event.target.classList.contains('recent')) {
-        //infoCard.addClass('is-hidden');
-        //$('#loading-icon').removeClass('is-hidden');
+        // Local Variables
         let recentName = event.target.id;
         let recall = JSON.parse(localStorage.getItem(recentName));
         let one = recall[0], two = recall[1], three = recall[2];
-        console.log(recall);
         let title = capitalizeFirstLetter(recentName);
+
+        //Hide Pokemon display card and invalid text, show loading icon
+        infoCard.addClass('is-hidden');
+        $('#loading-icon').removeClass('is-hidden');
+        $('.invalid').addClass('is-hidden');
+
+        //Display Info
         displayInfo(one, title);
         displayFlavor(two);
         displayCard(three);
+
+        //Show display Card, hide loading icon
+        infoCard.removeClass('is-hidden');
+        $('#loading-icon').addClass('is-hidden');
     }
 })
